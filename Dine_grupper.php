7d162d9fb7f7f8d6778940ba713ad9ba4030fce0
Jogga9 <100355272+Jogga9@users.noreply.github.com>
2022-02-24 14:10:39 +0100

    Add files via upload

---
+++ Dine_grupper.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dine Grupper</title>
+</head>
+<body>
+    <p>gruppene dine:</p>
+    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>Gruppe 1</button></a></p>
+    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>Gruppe 2</button></a></p>
+    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>Gruppe 3</button></a></p>
+    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>Gruppe 4</button></a></p>
+    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>Gruppe 5</button></a></p>
+</body>
+</html>
