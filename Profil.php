eb3deee918f804d8dc6b2605278976bb74b8f9ea
Jogga9 <100355272+Jogga9@users.noreply.github.com>
2022-02-24 13:57:17 +0100

    Add files via upload

---
+++ Profil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil</title>
+</head>
+<body>
+    <form action="Dine_grupper.php">
+        <input type="submit" value="Dine Grupper">
+    </form>
+    <br>
+    <p>dette er aktivitetene du er meldt på:</p>
+    <br>
+    <!--bruker POST for å gi info siden id til aktiviteten-->
+    <form action="InfoOmAktivitet.php">
+        <input type="submit" value="aktivitet 1">
+        <br>
+        <input type="submit" value="aktivitet 2">
+        <br>
+        <input type="submit" value="aktivitet 3">
+        <br>
+        <input type="submit" value="aktivitet 4">
+        <br>
+        <input type="submit" value="aktivitet 5">
+    </form>
+</body>
+</html>
