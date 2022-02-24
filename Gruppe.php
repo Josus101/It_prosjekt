6404843411e5f3fcb1783fb891f5d883280f89a3
Jogga9 <100355272+Jogga9@users.noreply.github.com>
2022-02-24 14:26:33 +0100

    Add files via upload

---
+++ Gruppe.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gruppe</title>
+</head>
+<body>
+    <form action="OpprettAktivitet.php" method="POST">
+    <input type="submit" value="Opprett Aktivitet">
+    </form>
+    <br>
+    <p>aktiviteter:</p>
+</body>
+</html>
