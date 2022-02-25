07b1d7e3b2f4a75e37ed5f2047c879c4e7acdbbe
Oscar Martins <48917892+BeaverOsa@users.noreply.github.com>
2022-02-25 10:55:37 +0100

    Update Gruppe.php

--- Gruppe.php
+++ Gruppe.php
@@ -5,10 +5,8 @@
     <title>Gruppe</title>
 </head>
 <body>
-    <p><a href="Dine_grupper.php"><button>Dine_grupper</button></a></p>
-    <form action="OpprettAktivitet.php" method="POST">
-        <input type="submit" value="Opprett Aktivitet">
-    </form>
+    <p><a href="dine_grupper.php"><button>Dine Grupper</button></a></p>
+    <p><a href="opprett_aktivitet.php"><button>Opprett Aktivitet</button></a></p>
     <br>
     <p>aktiviteter:</p>
 </body>
