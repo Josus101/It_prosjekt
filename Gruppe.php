07d0f3610b157cf0c663c65cb5830adeffb278ac
Jogga9 <100355272+Jogga9@users.noreply.github.com>
2022-02-25 10:54:05 +0100

    Update Gruppe.php

--- Gruppe.php
+++ Gruppe.php
@@ -5,8 +5,9 @@
     <title>Gruppe</title>
 </head>
 <body>
+    <p><a href="Dine_grupper.php"><button>Dine_grupper</button></a></p>
     <form action="OpprettAktivitet.php" method="POST">
-    <input type="submit" value="Opprett Aktivitet">
+        <input type="submit" value="Opprett Aktivitet">
     </form>
     <br>
     <p>aktiviteter:</p>
