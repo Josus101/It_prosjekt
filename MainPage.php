0d7d0179afa195d16c83c2d42824a925ac21819b
Wetle24 <100355471+Wetle24@users.noreply.github.com>
2022-02-25 10:06:55 +0100

    Update MainPage.php

--- MainPage.php
+++ MainPage.php
@@ -6,8 +6,8 @@
         <title>Hjemmeside</title>
     </head>
     <body>
-        <p><a href="Profil.php>Profil</a></p>
-        <p><a href="Logged_out.php>Profil</a></p>
+        <p><a href="Profil.php">Profil</a></p>
+        <p><a href="Logged_out.php">Logg ut</a></p>
         <p>Her skal det komme en feed</p>
     </body>
 </html>
