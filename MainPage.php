bba7250fc23460a0c638224737435e62cc521567
Jogga9 <100355272+Jogga9@users.noreply.github.com>
2022-02-25 10:48:31 +0100

    Update MainPage.php

--- MainPage.php
+++ MainPage.php
@@ -6,8 +6,8 @@
         <title>Hjemmeside</title>
     </head>
     <body>
-        <p><a href="Profil.php">Profil</a></p>
-        <p><a href="Logged_out.php">Logg ut</a></p>
+        <p><a href="Profil.php"><button>Profil</button></a></p> 
+        <p><a href="Logged_out.php"><button>Logg Out</button></a></p>
         <p>Her skal det komme en feed</p>
     </body>
 </html>
