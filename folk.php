db4c1ec339dbbfabdaf1e91af75d9873a42ad057
Josus101 <99477405+Josus101@users.noreply.github.com>
2022-02-25 10:49:03 +0100

    Update folk.php

--- folk.php
+++ folk.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // $user_id = $_SESSION['user_id'];
-$user_id = 2;
+$user_id = 1;
 
 $sql_username = "root";
 $sql_password = ""; // endre te "root" hvis du e på mac
@@ -22,25 +22,36 @@ echo "Connected successfully";
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!-- <link rel="style.css"> -->
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Folk</title>
 </head>
 <body>
+
+<h1>Folk</h1>
+
 <?php
-    $sql_select = "SELECT profile.name AS name, user_2_id
-                    FROM folk_du_har_vert_med, profile
-                    WHERE folk_du_har_vert_med.user_1_id = $user_id";
 
-    $result = mysqli_query($conn, $sql_select);
+$sql_select = "SELECT user_2_id, profile.name AS name
+FROM folk_du_har_vert_med, profile
+WHERE folk_du_har_vert_med.user_1_id = $user_id
+AND profile.id = folk_du_har_vert_med.user_2_id";
 
-    if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            echo "<br>id: " . $row["user_2_id"] . "name:" . $row["name"];
-        }
-    }else {
-        echo "0 results";
+$result = mysqli_query($conn, $sql_select);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    echo "<h2 id=\"nylige\">Nylige</h2>";
+    echo "<ul id=\"nylige_liste\">";
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<li>name:" . $row["name"] . "</li>";
     }
+    echo "</ul>";
+}else {
+    echo "0 results";
+}
+
+
+echo "<h2 id=\"dms\">DM's</h2>";
 
     mysqli_close($conn);
 ?>
