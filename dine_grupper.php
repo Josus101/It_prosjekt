5ced889be2535bdbda842328edd3911cb1d10d09
Jogga9 <100355272+Jogga9@users.noreply.github.com>
2022-02-25 11:14:36 +0100

    Update dine_grupper.php

--- dine_grupper.php
+++ dine_grupper.php
@@ -5,13 +5,13 @@
     <title>Dine Grupper</title>
 </head>
 <body>
-    <p><a href="profil.php"><button>Profil</button></a></p>
+    <p><a href="Profil.php"><button>Profil</button></a></p>
     <p>gruppene dine:</p>
     <!--sessions for å vita group id-->
-    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>gruppe 1</button></a></p>
-    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>gruppe 2</button></a></p>
-    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>gruppe 3</button></a></p>
-    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>gruppe 4</button></a></p>
-    <p><a href="http://localhost:8888/IT_projekt/Gruppe.php"><button>gruppe 5</button></a></p>
+    <p><a href="Gruppe.php"><button>Gruppe 1</button></a></p>
+    <p><a href="Gruppe.php"><button>Gruppe 2</button></a></p>
+    <p><a href="Gruppe.php"><button>Gruppe 3</button></a></p>
+    <p><a href="Gruppe.php"><button>Gruppe 4</button></a></p>
+    <p><a href="Gruppe.php"><button>Gruppe 5</button></a></p>
 </body>
 </html>
