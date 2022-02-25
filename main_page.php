bb76f8ac38652a4896b0be1bfe0e2f2367815138
Oscar Martins <48917892+BeaverOsa@users.noreply.github.com>
2022-02-25 10:52:07 +0100

    Update and rename MainPage.php to main_page.php

--- main_page.php
+++ main_page.php
@@ -6,8 +6,8 @@
         <title>Hjemmeside</title>
     </head>
     <body>
-        <p><a href="Profil.php"><button>Profil</button></a></p> 
-        <p><a href="Logged_out.php"><button>Logg Out</button></a></p>
+        <p><a href="profil.php"><button>Profil</button></a></p> 
+        <p><a href="logg_ut.php"><button>Logg Out</button></a></p>
         <p>Her skal det komme en feed</p>
     </body>
 </html>
