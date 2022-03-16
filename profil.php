9703097f9a645d5fac0ab1d7aca8e03dbe784c4d
Oscar Martins <48917892+BeaverOsa@users.noreply.github.com>
2022-03-16 19:59:32 +0100

    Update profil.php

--- profil.php
+++ profil.php
@@ -11,7 +11,7 @@
     <p>dette er aktivitetene du er meldt på:</p>
     <br>
     <!--bruker POST for å gi info siden id til aktiviteten-->
-    <form action="InfoOmAktivitet.php">
+    <form action="info_om_aktivitet.php">
         <input type="submit" value="aktivitet 1">
         <br>
         <input type="submit" value="aktivitet 2">
