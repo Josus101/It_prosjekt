a83a4cbcc66cca5ec4009742bb34fceef5d09f0d
Josus101 <99477405+Josus101@users.noreply.github.com>
2022-05-31 14:51:18 +0200

    Update create_acc.php

--- create_acc.php
+++ create_acc.php
@@ -1,7 +1,9 @@
 <?php
-    if (isset($_POST["name"])) {
+    require 'connect.php';
+
+    if (isset($_POST["navn"])) {
         $navn = $_POST["navn"];
-        $user = $_POST["user"];
+        $username = $_POST["username"];
         $passord = $_POST["passord"];
         $tlf = $_POST["tlf"];
         $universitet = $_POST["universitet"];
@@ -10,28 +12,23 @@
         $fagomraade = $_POST["fagomraade"];
         
         $interesser = [];
-        
-        $interesser[] = [$_POST["fotball"], 
-                         $_POST["innebandy"], 
-                         $_POST["kampsport"], 
-                         $_POST["gaming"], 
-                         $_POST["slalom"], 
-                         $_POST["langrenn"], 
-                         $_POST["klatring"], 
-                         $_POST["surfing"], 
-                         $_POST["parkour"], 
-                         $_POST["basketball"], 
-                         $_POST["volleyball"], 
-                         $_POST["skateboarding"], 
-                         $_POST["sying"], 
-                         $_POST["vektloefting"], 
-                         $_POST["vektloefting"], 
-                         $_POST["paintball"]];
-                              
-        require 'connect.php';
-        
+
+        $sql = "SELECT navn FROM interesser";
+
+        $result = mysqli_query($conn, $sql);
+
+        if ( mysqli_num_rows($result) > 0) {          
+            while($row = mysqli_fetch_assoc($result)) {
+                if(isset($_POST[$row['navn']])){
+                    array_push($interesser, $_POST[$row['navn']]);
+                }
+            }
+        }  
+        else{
+            echo "no work sadge";
+        }
         $sql = "INSERT INTO user (username, password) 
-                VALUES (\"$user\", \"$passord\")";
+                VALUES (\"$username\", \"$passord\")";
         if (mysqli_query($conn, $sql)) {
             $last_id = mysqli_insert_id($conn);
         }
@@ -41,15 +38,16 @@
         
         $sql = "INSERT INTO profile (user_id, navn, tlf, universitet, alder, kjoenn, fagomraade) 
                 VALUES ($last_id, \"$navn\", $tlf, $universitet, $alder, $kjoenn, $fagomraade)";
-        (mysqli_query($conn, $sql));
         
         for ($i = 0; $i < count($interesser); $i++) {
-            if ($interesser[$i] == true) {
-                $index = $i + 1;
-                $sql = "INSERT INTO interesser_user (user_id, interesser_id) 
-                VALUES($last_id, $index)";
-                (mysqli_query($conn, $sql));
-            }
+            $index = $i + 1;
+            $sql = "INSERT INTO interesser_user (user_id, interesser_id) 
+            VALUES($last_id, $index)";
+            if (mysqli_query($conn, $sql)) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            } 
         }
     }
 ?>
