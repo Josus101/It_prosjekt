282678128eb031052c7892d41bc249f75ed8813b
Josus101 <99477405+Josus101@users.noreply.github.com>
2022-06-02 20:26:45 +0200

    Update aksepter_aktivitet.php

--- aksepter_aktivitet.php
+++ aksepter_aktivitet.php
@@ -1,13 +1,17 @@
 <?php
 session_start();
 $user_id = $_SESSION['user_id'];
-$aktivitet_id = $_GET['id'];
+$aktivitet_id = $_GET['aktivitet_id'];
 require 'connect.php';
 
 $sql = "INSERT INTO aktivitet_users (aktivitet_id, user_id) VALUES ($aktivitet_id, $user_id)";
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "New record created successfully <br>";
+    echo "Du har akseptert aktiviteten";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 } 
 ?>
+<head>
+    <meta http-equiv="refresh" content="2; url='aktiviteter.php'">
+</head>
