558dd4d828d1b3d9ef9672dc5db1711a0fce944b
Josus101 <99477405+Josus101@users.noreply.github.com>
2022-06-02 20:29:28 +0200

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -2,23 +2,12 @@
 
     session_start();
 
-
-    if ( isset( $_POST["username"] ) && isset( $_POST["password"] ) )
-    {
+    $login = false;
+    if ( isset( $_POST["username"] ) && isset( $_POST["password"] ) ) {
         $user = $_POST["username"];
         $pass = $_POST["password"];
 
-        $sqlserver   = "mysql.elev.stolav.it";
-        $sqlusername = "stolav_universus";
-        $sqlpassword = "3mMiNv!n";
-        $sqldatabase = "stolav_universus";
-
-        $conn = mysqli_connect($sqlserver, $sqlusername, $sqlpassword, $sqldatabase);
-
-        mysqli_set_charset( $conn, "utf8" );
-
-        if ( !$conn ) { die("Server down. Please try again later."); }
-
+        require 'connect.php';
 
         $sql = "SELECT * FROM user
                 WHERE username = \"$user\"
@@ -27,20 +16,18 @@
         $result = mysqli_query($conn, $sql);
 
         if ( mysqli_num_rows($result) == 1 ) {          
-          $_SESSION["username"] = $user;
-          while($row = mysqli_fetch_assoc($result)) {
-            $_SESSION["user_id"] = $row["id"];
-          }
-          echo "Login OK";
-        }  
-
-        else {
+            while($row = mysqli_fetch_assoc($result)) {
+                $_SESSION["user_id"] = $row["id"];
+            }
+            echo "Login OK";
+            $login = true;
+        }else {
           session_destroy();
           echo "Login failed";
         }
     }
 
-  else {
+    else{
         session_destroy();
         echo "Login failed!";
     }
@@ -52,7 +39,14 @@
     <head>
         <title>Authenticate</title>
         <meta charset="utf-8">
-        <meta http-equiv="refresh" content="0; url='main_page.php'">
+<?php
+if($login){
+    echo "<meta http-equiv=\"refresh\" content=\"2; url='main_page.php'\">";
+}
+else{
+    echo "<meta http-equiv=\"refresh\" content=\"2; url='main_page.php'\">";
+}
+?>
     </head>
     <body>
     </body>
