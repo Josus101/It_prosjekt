c8c707581ca6f69b0f81c3fc1419e8539466bb5b
Josus101 <99477405+Josus101@users.noreply.github.com>
2022-06-03 09:17:52 +0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -9,16 +9,17 @@
         <img src="Universus.png" alt="Logo Universus">
         <ul>
             <?php
-                $urls = ["main_page.php", "dine_grupper.php", "aktiviteter.php", "folk.php"];
+                $urls = ["/main_page.php", "/dine_grupper.php", "/aktiviteter.php", "/folk.php"];
+                $names = ["Main page", "Dine grupper", "Aktiviteter", "Folk"];
 
                 $active = $_SERVER["REQUEST_URI"];
                 
                 for ($i = 0; $i < count($urls); $i++) {
-                    if ("/" . $urls[$i] == $active) {
-                        echo "<li><a class=\"active\" href=\"\\$urls[$i]\">$urls[$i]</a></li>";
+                    if ($urls[$i] == $active) {
+                        echo "<li><a class=\"active\" href=\"$urls[$i]\">$names[$i]</a></li>";
                     }
                     else {
-                        echo "<li><a href=\"$urls[$i]\">$urls[$i]</a></li>";
+                        echo "<li><a href=\"$urls[$i]\">$names[$i]</a></li>";
                     }
                 }
             ?>
